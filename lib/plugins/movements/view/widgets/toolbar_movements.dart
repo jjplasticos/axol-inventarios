8b687f0b749e221635a312eb1add997e4bc7cc8f
Daniel LMac <lozano.morales.daniel@outlook.com>
2023-06-20 19:30:28 -0700

    Historial de movimientos 0.13

--- lib/plugins/movements/view/widgets/toolbar_movements.dart
+++ lib/plugins/movements/view/widgets/toolbar_movements.dart
@@ -1,6 +1,14 @@
+import 'dart:typed_data';
+
 import 'package:axol_inventarios/models/textfield_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 import '../../../../global_widgets/toolbar.dart';
 import '../../../../models/elemnets_bar_model.dart';
@@ -51,6 +59,30 @@ class ToolbarMovements extends StatelessWidget {
           icon: const Icon(Icons.restart_alt),
           action: () {
             context.read<MovementsCuibit>().loadList();
+          },
+        ),
+        ElementsBarModel(
+          text: null,
+          icon: const Icon(Icons.print),
+          action: () async {
+            /*final Uint8List fontData = File('open-sans.ttf').readAsBytesSync();
+            final ttf = pw.Font.ttf(fontData.buffer.asByteData());*/
+            /*final font = await rootBundle.load("assets/open-sans.ttf");
+            final ttf = pw.Font.ttf(font);*/
+            final font = await PdfGoogleFonts.nunitoExtraLight();
+
+            final pdf = pw.Document();
+            pdf.addPage(pw.Page(
+              pageFormat: PdfPageFormat.a4,
+              build: (pw.Context context) {
+                return pw.Center(
+                  child: pw.Text('Hola mundo!', style: pw.TextStyle(font: font, fontSize: 40),)
+                );
+              }
+            ));
+            final output = await getTemporaryDirectory();
+            final outputFile = File('${output.path}/holaMundo.pdf');
+            await outputFile.writeAsBytes(await pdf.save());
           },
         )
       ],
